36bae54ae6b17de6c431205049dc335354fe7779
Radar31520 <godsprop7@hotmail.com>
2012-01-07 17:33:41 -0500

    SignIn Failure

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "Invalid Email/Passord Combination"
+       @title = "SignIn"
+      render 'new'
+    else
+      #handle successful signin
+    end
   end
 
   def destroy
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,6 +4,7 @@ describe SessionsController do
   render_views
 
   describe "GET 'new'" do
+
     it "it should be succesful" do
       get 'new'
       response.should be_success
@@ -13,7 +14,32 @@ describe SessionsController do
       get 'new'
       response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | SignIn")
     end
+  end
 
+  describe "POST 'create'" do
+
+    describe "failure" do
+
+      before(:each) do
+        @attr = {:email => "" , :password => ""}
+      end
+
+      it "should render the 'new' page " do
+        post :create, :session => @attr
+        response.should render_template('new')
+      end
+
+      it "should render the right title" do
+        post :create, :session => @attr
+        response.should have_selector('title', :content => "SignIn")
+      end
+
+      it "should have an error message" do
+        post :create, :session => @attr
+        flash.now[:error].should =~ /invalid/i
+      end
+
+    end
   end
 
 end

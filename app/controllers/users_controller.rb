6adc7ef65e7d9eb51387940f30b5113dfd32058f
Radar31520 <godsprop7@hotmail.com>
2012-01-07 12:55:09 -0500

    finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,20 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
     @title = "SignUp"
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the sample app!"
+      redirect_to @user
+    else
+      @title = "SignUp"
+      render 'new'
+    end
+
+  end
 end
 
